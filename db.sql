78ca88b2904fd38ae81745b86f2ce3dc10c5089e
surenpoghosian <surenpoghosyan17@gmail.com>
2022-12-24 15:12:55 +0400

    db

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE bug-bounty;
-
+CREATE DATABASE bug_bounty;
+USE bug_bounty;
 CREATE TABLE projects (
     id int AUTO_INCREMENT,
     organization VARCHAR(255),
@@ -9,14 +9,14 @@ CREATE TABLE projects (
     reward_to int NOT NULL,
     image_url VARCHAR(255),
     created_at int,
-    PRIMARY KEY id
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE users (
     id int AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    type ENUM NOT NULL,
-    PRIMARY KEY id
+    type ENUM("0","1","2") Default "0" NOT NULL,
+    PRIMARY KEY(id)
 );
 
