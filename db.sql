2f5438bf8b22b7a7819a2844269ac7640df45569
JSHayk <jshyakdeveloper@gmail.com>
2022-12-24 15:14:05 +0400

    added project service

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE bug-bounty;
+CREATE DATABASE bug_bounty;
 
 CREATE TABLE projects (
     id int AUTO_INCREMENT,
@@ -16,7 +16,7 @@ CREATE TABLE users (
     id int AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    type ENUM NOT NULL,
+    type ENUM(0, 1, 2) NOT NULL,
     PRIMARY KEY id
 );
 
